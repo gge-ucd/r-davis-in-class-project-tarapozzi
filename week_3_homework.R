e474f23c255f82e98c837b3af7d9bab53ea02004
Tara Pozzi <tarapozzi@campus-026-193.ucdavis.edu>
2021-10-14 14:58:48 -0700

    hw updates from class

--- week_3_homework.R
+++ week_3_homework.R
@@ -2,11 +2,15 @@
 # Load survey data 
 surveys <- read.csv("data/portal_data_joined.csv")
 
-# subset data
-surveys_base <- surveys[c("species_id", "weight", "plot_type")] 
+# create a new survey data
+surveys_base <- surveys[c("species_id", "weight", "plot_type")] #base R
+surveys_base <- select(surveys, species_id, weight, plot_type)
+surveys_base
+
 
 # first 60 rows
-surveys_base <- surveys_base[1:60,]
+surveys_base <- surveys_base[1:60,] #baseR # might need to concatenate it if you have characters
+surveys_base <- head(surveys_base, 60)
 
 # convert species id and plot type to factors
 surveys_base$plot_type <- as.factor(surveys_base$plot_type)
@@ -15,8 +19,17 @@ surveys_base$species_id <- as.factor(surveys_base$species_id)
 str(surveys_base)
 
 # remove na's
-surveys_base <- na.omit(surveys_base)
+surveys_base <- surveys_base %>%
+  na.omit(surveys_base$weight) #OR
+
+surveys_base <- na.omit(surveys_base, surveys_base$weight)
+
+surveys_base <- surveys_base[!is.na(surveys_base$weight),] # another way to remove the na's 
+
+#another way to do it
+complete.cases(surveys_bases)
 
 # challenge
 challenge_base <- surveys_base[(surveys_base[,2] > 150),]
+
 
