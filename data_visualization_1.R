5f82981a4b2f8794b4a93e3631f3d3345cc7eb2d
Tara Pozzi <tarapozzi@campus-008-005.ucdavis.edu>
2021-10-28 16:19:46 -0700

    updates from class

--- data_visualization_1.R
+++ data_visualization_1.R
@@ -44,6 +44,27 @@ base_plot2 +
   geom_boxplot() +
   geom_jitter(alpha = 0.2, aes(color = species_id)) # since it is just specified in jitter it only colors the points and not the boxplots
 
+## Challenge: Use what you just learned to create a scatter plot of weight and 
+#species_id with weight on the Y-axis, and species_id on the X-axis. 
+#Have the colors be coded by plot_type. 
+#Is this a good way to show this type of data? What might be a better graph?
+
+challenge.plot <- ggplot(surveys, aes(species_id, weight)) +
+  geom_point(alpha = 0.3, aes(color=plot_type))
+
+challenge.plot # this doesn't seem super helpful maybe a boxplot would be better?
+
+challenge.plot2 <- ggplot(surveys, aes(species_id, weight)) +
+  geom_point() +
+  facet_wrap(~ plot_type)
+
+challenge.plot2 
+
+ggplot(data = surveys, mapping = aes(x = species_id, y = weight)) +
+  geom_boxplot(alpha = 0) +
+  geom_jitter(alpha = 0.3, color = "tomato") #notice our color needs to be in quotations
+
+
 ## data visualization part 1b
 surveys_complete <- read.csv("data/portal_data_joined.csv") %>%
   filter(complete.cases(.))
@@ -68,3 +89,28 @@ ggplot(yearly.counts, aes(year, n, group = species_id)) + # make sure to tell gg
 
 library(ggthemes) # has a lot of pre-established themes. looks super useful
 
+## Challenge 2
+# boxplot does show the shape of the data, use violin to show the distribution
+ggplot(surveys, mapping = aes(x = species_id, y = weight)) +
+  geom_jitter(alpha = 0.3, color = "tomato") +
+  geom_violin(alpha = 0)
+
+# try changing the scale fo the axis to see what effect this has on the data
+ggplot(surveys, mapping = aes(x = species_id, y = weight)) +
+  geom_jitter(alpha = 0.3, color = "tomato") +
+  geom_violin(alpha = 0) +
+  scale_y_log10()
+
+# now make a new plot looking at hindfoot_length for species NL and PF
+surveys %>% 
+  filter(species_id == "NL" | species_id == "PF") %>% 
+  ggplot(mapping = aes(x= species_id, y = hindfoot_length)) + # remove call for dataset because it's piped
+  geom_jitter(alpha = 0.3, aes(color = as.factor(plot_id))) + # we want to color the plot_id, need to do as factor becuase ggplot sees it as a continue variable instead of a "categorical" number
+  geom_boxplot()
+
+surveys %>% 
+  filter(species_id == "NL" | species_id == "PF") %>% 
+  mutate(plot_factor = as.factor(plot_id)) %>% # could also create a new column for plot_id as a factor
+  ggplot(mapping = aes(x= species_id, y = hindfoot_length)) + # remove call for dataset because it's piped
+  geom_jitter(alpha = 0.3, aes(color = as.factor(plot_factor))) + # we want to color the plot_id, need to do as factor becuase ggplot sees it as a continue variable instead of a "categorical" number
+  geom_boxplot()
--- week_5_homework.R
+++ week_5_homework.R
@@ -6,16 +6,15 @@ surveys <- read_csv("data/portal_data_joined.csv")
 # question 1: new data frame with plot type as unique columns 
 surveys.wide <- surveys %>%
   filter(!is.na(hindfoot_length)) %>%
-  group_by(genus, plot_id) %>%
-  summarize(mean_length = mean(hindfoot_length)) %>%
-  pivot_wider(., names_from = 'plot_id', values_from = 'mean_length') %>%
+  group_by(genus, plot_type) %>%
+  summarize(mean_length = mean(hindfoot_length)) %>% # this creates a new column for mean length 
+  pivot_wider(., names_from = 'plot_type', values_from = 'mean_length') %>% # names from becomes the column names and values from fills in the value 
   arrange(Control) # sorted by values int eh control type. I don't understand this part of the code
-
-
 
 # question 2
 summary(surveys$weight)
 
+#if else and case when handle na's differently so make sure to look into this when i use these functions
 surveys <- mutate(surveys, 
                   weight_cat = ifelse(surveys$weight <= 20, "small",
                                       ifelse(surveys$weight > 20 & surveys$weight < 48, "medium", 
